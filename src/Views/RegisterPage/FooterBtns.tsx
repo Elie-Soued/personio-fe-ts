2f973cee491959257016b3f5e2bf635507739771
Elie Soued <eliegebransoued@gmail.com>
2023-01-28 23:35:11 +0200

    create footerBtns

---
+++ src/Views/RegisterPage/FooterBtns.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface Props {
+  backToLoginFn: Function;
+}
+
+export default function FooterBtns({ backToLoginFn }: Props) {
+  return (
+    <div className=" d-flex flex-column  justify-content-center  pt-1 mb-5 pb-1 col-12 h-25">
+      <div className="d-flex justify-content-around p-2 ">
+        <button
+          className="btn btn-primary btn-block fa-lg  mb-3 active w-10"
+          type="button"
+          onClick={() => {
+            backToLoginFn();
+          }}
+        >
+          Go Back to Log in
+        </button>
+        <button
+          className="btn btn-secondary btn-block fa-lg  mb-3  w-10"
+          type="button"
+          disabled
+        >
+          Create Profile
+        </button>
+      </div>
+    </div>
+  );
+}
