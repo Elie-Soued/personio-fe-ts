c85a2b5d22a0a5a8e0932116dcf0dc9fc4136ca0
Elie Soued <eliegebransoued@gmail.com>
2023-01-28 19:16:06 +0200

    add the next page and previous page button to the register page

--- src/Views/RegisterPage/RegisterPage.tsx
+++ src/Views/RegisterPage/RegisterPage.tsx
@@ -1,43 +1,58 @@
-import React, { useState } from "react";
-import { Row, Col, Navbar, Nav } from "react-bootstrap";
+import React, { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import undersurface from "../../images/undersurface.jpg";
-import passion from "../../images/passion.jpg";
 import king from "../../images/king.jpg";
-import responsability from "../../images/responsability.jpg";
 import { Section } from "./Section";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { MDBContainer } from "mdb-react-ui-kit";
-interface ISectionProps {
-  activeSection: string;
-  setActiveSection: React.Dispatch<React.SetStateAction<string>>;
-}
 
-function renderActiveSection(activeSection: string) {
-  return <Section name={activeSection} attributes />;
-}
+const sections = [
+  "Public Profile",
+  "Hr Information",
+  "Personal Data",
+  "Payroll Information",
+  "Bank Details",
+  "Emergency Contact",
+  "Employee Equipment",
+  "Development",
+];
 
-const RegisterPage: React.FC<ISectionProps> = ({
-  activeSection,
-  setActiveSection,
-}) => {
-  const sections = [
-    "Public Profile",
-    "Hr Information",
-    "Personal Data",
-    "Payroll Information",
-    "Bank Details",
-    "Emergency Contact",
-    "Employee Equipment",
-    "Development",
-  ];
+export default function RegisterPage() {
+  const [index, setIndex] = useState(0);
+  const [activeSection, setActiveSection] = useState(sections[index]);
 
-  const navigate = useNavigate();
+  const goToPreviousPage = useCallback(() => {
+    if (index === 0) {
+      return;
+    }
+    setIndex((prevIndex) => prevIndex - 1);
+  }, [index, setIndex]);
 
-  const backToLogin = () => {
-    navigate("/");
-  };
+  const goToNextPage = useCallback(() => {
+    if (index === 7) {
+      return;
+    }
+    setIndex((prevIndex) => prevIndex + 1);
+  }, [index, setIndex]);
 
-  console.log(activeSection);
+  useEffect(() => {
+    function handleKeyPress(event: any) {
+      if (event.key === "ArrowRight") {
+        goToNextPage();
+      } else if (event.key === "ArrowLeft") {
+        goToPreviousPage();
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    setActiveSection(sections[index]);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [index, goToNextPage, goToPreviousPage]);
 
   return (
     <div
@@ -45,7 +60,7 @@ const RegisterPage: React.FC<ISectionProps> = ({
       style={{
         backgroundColor: "#eee",
         border: "4px solid white",
-        height: "100vh",
+        height: "100%",
       }}
     >
       <MDBContainer
@@ -58,8 +73,33 @@ const RegisterPage: React.FC<ISectionProps> = ({
       >
         <div className="row g-0">
           <div className="d-flex align-items-center col-lg-6 ">
-            <div className="d-flex flex-column w-100">
+            <div className=" w-100">
               {renderActiveSection(activeSection)}
+
+              {/**Here is the button section */}
+
+              <div className="d-flex justify-content-center">
+                <div className="col-6 text-center p-2">
+                  <button
+                    className="btn btn-primary btn-block fa-lg   active w-10"
+                    onClick={() => {
+                      goToPreviousPage();
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faArrowLeft} size="lg" />
+                  </button>
+                </div>
+                <div className="col-6 text-center p-2">
+                  <button
+                    className="btn btn-primary btn-block fa-lg  mb-3 active w-10"
+                    onClick={() => {
+                      goToNextPage();
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faArrowRight} size="lg" />
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
 
@@ -87,6 +127,8 @@ const RegisterPage: React.FC<ISectionProps> = ({
       </MDBContainer>
     </div>
   );
-};
+}
 
-export default RegisterPage;
+function renderActiveSection(activeSection: string) {
+  return <Section name={activeSection} attributes />;
+}
