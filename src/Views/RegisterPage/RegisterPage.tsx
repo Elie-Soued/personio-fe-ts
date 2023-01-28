74df36513693b6268ad9f2dba9b06e35ebe56444
Elie Soued <eliegebransoued@gmail.com>
2023-01-28 23:36:55 +0200

    update register page

--- src/Views/RegisterPage/RegisterPage.tsx
+++ src/Views/RegisterPage/RegisterPage.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useContext } from "react";
 import undersurface from "../../images/undersurface.jpg";
 import king from "../../images/king.jpg";
-import { Section } from "./Section";
+import Section from "./Section";
 import { MDBContainer } from "mdb-react-ui-kit";
 import NagigationBtns from "./NavigationBtn";
+import FooterBtns from "./FooterBtns";
+import { useNavigate } from "react-router-dom";
+import { employee } from "./Employee";
 
 const sections = [
   "Public Profile",
@@ -16,9 +19,17 @@ const sections = [
   "Development",
 ];
 
+export const EmployeeContext = React.createContext(employee);
+
 export default function RegisterPage() {
   const [index, setIndex] = useState(0);
   const [activeSection, setActiveSection] = useState(sections[index]);
+
+  const navigate = useNavigate();
+
+  const backToLogin = () => {
+    navigate("/");
+  };
 
   const goToPreviousPage = useCallback(() => {
     if (index === 0) {
@@ -52,59 +63,64 @@ export default function RegisterPage() {
     };
   }, [index, goToNextPage, goToPreviousPage]);
 
+  console.log("employee in registerPage", employee);
+
   return (
-    <div
-      className="p-0"
-      style={{
-        backgroundColor: "#eee",
-        border: "4px solid white",
-        height: "100%",
-      }}
-    >
-      <MDBContainer
-        className="my-0 gradient-form h-auto"
+    <EmployeeContext.Provider value={employee}>
+      <div
+        className="p-0"
         style={{
-          backgroundColor: "white",
-          width: "auto",
-          padding: 0,
+          backgroundColor: "#eee",
+          border: "4px solid white",
+          height: "100%",
         }}
       >
-        <div className="row g-0">
-          <div className="d-flex align-items-center col-lg-6 ">
-            <div className=" w-100">
-              {renderActiveSection(activeSection)}
+        <MDBContainer
+          className="my-0 gradient-form h-auto"
+          style={{
+            backgroundColor: "white",
+            width: "auto",
+            padding: 0,
+          }}
+        >
+          <div className="row g-0">
+            <div className="d-flex align-items-center col-lg-6 ">
+              <div className=" w-100">
+                {renderActiveSection(activeSection)}
 
-              <NagigationBtns
-                nextFn={goToNextPage}
-                prevFn={goToPreviousPage}
-                section={activeSection}
-              ></NagigationBtns>
+                <NagigationBtns
+                  nextFn={goToNextPage}
+                  prevFn={goToPreviousPage}
+                  section={activeSection}
+                ></NagigationBtns>
+              </div>
+            </div>
+
+            <div className="rounded-top col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div className="d-flex flex-column  align-items-center gradient-custom-2 h-100">
+                <img
+                  className="w-100"
+                  src={king}
+                  style={{ borderRadius: "5px" }}
+                  alt="King"
+                ></img>
+                <FooterBtns backToLoginFn={backToLogin}></FooterBtns>
+              </div>
             </div>
           </div>
-
-          <div className="rounded-top col-lg-6 d-flex align-items-center gradient-custom-2">
-            <div className="d-flex align-items-center gradient-custom-2 h-100">
+          <div className="col-xl-12">
+            <div className="col-xl-12">
               <img
                 className="w-100"
-                src={king}
+                src={undersurface}
                 style={{ borderRadius: "5px" }}
-                alt="King"
+                alt="footer"
               ></img>
             </div>
           </div>
-        </div>
-        <div className="col-xl-12">
-          <div className="col-xl-12">
-            <img
-              className="w-100"
-              src={undersurface}
-              style={{ borderRadius: "5px" }}
-              alt="footer"
-            ></img>
-          </div>
-        </div>
-      </MDBContainer>
-    </div>
+        </MDBContainer>
+      </div>
+    </EmployeeContext.Provider>
   );
 }
 
