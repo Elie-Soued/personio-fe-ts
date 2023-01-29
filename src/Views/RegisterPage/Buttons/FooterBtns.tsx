52ac4418bea78c9b949ae0908f4dba773d97cdaf
Elie Soued <eliegebransoued@gmail.com>
2023-01-29 21:52:05 +0200

    add show context to the footerBtns

--- src/Views/RegisterPage/Buttons/FooterBtns.tsx
+++ src/Views/RegisterPage/Buttons/FooterBtns.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useContext } from "react";
+import { EmployeeContext } from "../EmployeeContext";
 
 interface Props {
   backToLoginFn: Function;
 }
 
 export default function FooterBtns({ backToLoginFn }: Props) {
+  const { employee } = useContext(EmployeeContext);
+
+  const showContext = () => {
+    console.log(employee, "employee in footer");
+  };
+
   return (
     <div className=" d-flex flex-column  justify-content-center  pt-1 mb-5 pb-1 col-12 h-25">
       <div className="d-flex justify-content-around p-2 ">
@@ -20,7 +28,9 @@ export default function FooterBtns({ backToLoginFn }: Props) {
         <button
           className="btn btn-secondary btn-block fa-lg  mb-3  w-10"
           type="button"
-          disabled
+          onClick={() => {
+            showContext();
+          }}
         >
           Create Profile
         </button>
