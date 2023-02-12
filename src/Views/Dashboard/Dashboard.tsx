034dabc8f26b6c41b1d744992341ca8107b79d87
Elie Soued <eliegebransoued@gmail.com>
2023-02-12 14:49:10 +0200

    add a go back button to the dashboard

--- src/Views/Dashboard/Dashboard.tsx
+++ src/Views/Dashboard/Dashboard.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Dashboard() {
-  const user = localStorage.getItem("user");
-  return <h1>Hello {user} this is the dashboard</h1>;
+    const navigate = useNavigate();
+    const backToRegister = () => {
+        navigate('/register');
+    };
+
+    return (
+        <div>
+            <h1>Hello ouf this is the dashboard</h1>
+            <button
+                onClick={() => {
+                    backToRegister();
+                }}
+            >
+                go back to register
+            </button>
+        </div>
+    );
 }
