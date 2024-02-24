8771d9edf384967e71548b1ed2ef61c723abbc7e
elie <elie.soued@tech11.com>
2024-02-24 20:46:03 +0100

    fetch user data when getting to the dashboard

--- src/Utils/ServiceUtils.tsx
+++ src/Utils/ServiceUtils.tsx
@@ -1,35 +1,26 @@
-import axios from "axios";
+import axios from 'axios';
 
 // URL for Development
 
-const URL = "http://localhost:5000/users";
-const URLRegister = "http://localhost:5000/users/register";
-const URLLogin = "http://localhost:5000/users/login";
+const URL = 'http://localhost:5000/users';
+const URLRegister = 'http://localhost:5000/users/register';
+const URLLogin = 'http://localhost:5000/users/login';
 
-type httpVerb =
-  | "head"
-  | "options"
-  | "put"
-  | "post"
-  | "patch"
-  | "delete"
-  | "get";
+type httpVerb = 'head' | 'options' | 'put' | 'post' | 'patch' | 'delete' | 'get';
 
-const doRequest = async (httpVerb: httpVerb, url: any, payload: any) => {
-  const token = localStorage.getItem("token");
+const doRequest = async (httpVerb: httpVerb, url: any, payload?: any) => {
+    const token = localStorage.getItem('token');
 
-  const headers = {
-    "Content-Type": "application/json;charset=utf-8",
-    Authorization: token,
-    "Access-Control-Allow-Origin": "*",
-  };
+    if (token) axios.defaults.headers[httpVerb]!['authorization'] = token;
+    axios.defaults.headers[httpVerb]!['Content-Type'] = 'application/json;charset=utf-8';
+    axios.defaults.headers[httpVerb]!['Access-Control-Allow-Origin'] = '*';
 
-  try {
-    const response = await axios[httpVerb](url, payload, { headers });
-    return response;
-  } catch (error) {
-    console.log(error, "DB might be out of sync");
-  }
+    try {
+        const response = await axios[httpVerb](url, payload);
+        return response;
+    } catch (error) {
+        console.log(error, 'DB might be out of sync');
+    }
 };
 
 export { doRequest, URL, URLRegister, URLLogin };
--- src/Views/Dashboard/Dashboard.tsx
+++ src/Views/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { doRequest, URL } from '../../Utils/ServiceUtils';
 
 export default function Dashboard() {
     let { username } = useParams();
@@ -8,6 +9,15 @@ export default function Dashboard() {
     const backToRegister = () => {
         navigate('/register');
     };
+
+    const getUserData = async () => {
+        const response: any = await doRequest('get', URL);
+        console.log(response);
+    };
+
+    useEffect(() => {
+        getUserData();
+    }, []);
 
     return (
         <div>
