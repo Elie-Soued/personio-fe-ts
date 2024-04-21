46e90a14bd67dd1450a7195e44ff5f71a573c44a
elie <elie.soued@tech11.com>
2024-04-21 22:56:26 +0200

    adding the admin route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import LandingPage from "./Views/LandingPage/LandingPage";
-import Dashboard from "./Views/Dashboard/Dashboard";
-import RegisterPage from "./Views/RegisterPage/RegisterPage";
+import LandingPage from './Views/LandingPage/LandingPage';
+import Dashboard from './Views/Dashboard/Dashboard';
+import RegisterPage from './Views/RegisterPage/RegisterPage';
+import AdminPage from './Views/AdminPage/AdminPage';
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard/:username" element={<Dashboard />}></Route>
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/dashboard/:username" element={<Dashboard />}></Route>
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/admin/:username" element={<AdminPage />}></Route>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
