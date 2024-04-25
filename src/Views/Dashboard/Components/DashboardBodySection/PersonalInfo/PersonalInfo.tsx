e6c97082c029f1666f7ef402179088ec543bcec8
elie <elie.soued@tech11.com>
2024-04-25 17:47:38 +0200

    refactor personalInfo

--- src/Views/Dashboard/Components/DashboardBodySection/PersonalInfo/PersonalInfo.tsx
+++ src/Views/Dashboard/Components/DashboardBodySection/PersonalInfo/PersonalInfo.tsx
@@ -1,22 +1,27 @@
+import { useState } from 'react';
 import PersonalInfoNavBar from './PersonalInfoNavBar';
 import PersonalInfoDisplay from './PersonalInfoDisplay';
-import { IEmployeeProfile } from '../../../../RegisterPage/Interface/IEmployeeProfile';
+import { EmployeeProfileInterface, EmployeeContext } from '../../../../../Constants/Constants';
 
 interface Props {
-    userData: IEmployeeProfile;
+    userData: EmployeeProfileInterface;
 }
 
 export default function PersonalInfo({ userData }: Props) {
+    const [employee, setEmployee] = useState<EmployeeProfileInterface>(userData);
+
+    const updateEmployee = (employeeUpdated: EmployeeProfileInterface) => {
+        setEmployee(employeeUpdated);
+    };
     return (
-        <div className="row g-0" style={{ border: '4px dashed black', height: '100%' }}>
+        <div className="row g-0" style={{ height: 'auto' }}>
             <div className={"d-flex align-items-center col-md-3 col-12 ${h-md-100 h-25'}"}>
                 <PersonalInfoNavBar />
             </div>
-            <div
-                className={"d-flex align-items-center col-md-9 col-12 ${'h-md-100 h-75'}"}
-                style={{ border: '4px dashed blue' }}
-            >
-                <PersonalInfoDisplay userData={userData} />
+            <div className={"d-flex align-items-center col-md-9 col-12 ${'h-md-100 h-75'}"}>
+                <EmployeeContext.Provider value={{ employee, updateEmployee }}>
+                    <PersonalInfoDisplay />
+                </EmployeeContext.Provider>
             </div>
         </div>
     );
