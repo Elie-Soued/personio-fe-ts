326c08b012ad396160a0e8ac034538732d3e1431
elie <elie.soued@tech11.com>
2024-04-27 00:29:52 +0200

    adding a logout button in the dashboard view

--- src/Views/Dashboard/Components/DashboardNavBar.tsx
+++ src/Views/Dashboard/Components/DashboardNavBar.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar, faEnvelope, faHouse, faMagnifyingGlass, faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCalendar,
+    faEnvelope,
+    faHouse,
+    faMagnifyingGlass,
+    faUserFriends,
+    faArrowLeftRotate,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function DashboardNavbar() {
-    const sections = ['Home', 'Search', 'Inbox', 'Employees', 'Calendar'];
-    const icons = [faHouse, faMagnifyingGlass, faEnvelope, faUserFriends, faCalendar];
+    const sections = ['Home', 'Search', 'Inbox', 'Employees', 'Calendar', 'Logout'];
+    const icons = [faHouse, faMagnifyingGlass, faEnvelope, faUserFriends, faCalendar, faArrowLeftRotate];
 
     return (
         <ul className=" w-100 h-100  d-flex flex-row flex-md-column p-3" style={{ listStyle: 'none' }}>
             {sections.map((section, index) => (
-                <li className="mb-2">
+                <li
+                    className="mb-2"
+                    onClick={() => {
+                        if (section === 'Logout') {
+                            localStorage.removeItem('token');
+                            window.location.href = '/';
+                        }
+                    }}
+                >
                     <a href="" style={{ textDecoration: 'none', color: 'inherit' }}>
                         <FontAwesomeIcon icon={icons[index]} />
                         <span className="m-2">{section}</span>
