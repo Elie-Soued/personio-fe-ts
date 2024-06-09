989128b8bba033f6517c1c6fb32a22c8e85a0ab9
elie <elie.soued@tech11.com>
2024-06-09 18:54:37 +0200

    add the mechanism to upload a profile picture

--- src/Views/Dashboard/Components/DashBoardHeaderInfo.tsx
+++ src/Views/Dashboard/Components/DashBoardHeaderInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { doRequest } from '../../../Utils/ServiceUtils';
 import { faBriefcase, faMapMarker, faUsers, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,11 +8,37 @@ export default function DashboardHeaderInfo(userData: EmployeeProfileType) {
     const { position, team, department, office, first_name, last_name } = userData.public;
     const { supervisor, hire_date } = userData.hrInformation;
 
+    const [profilePicture, setProfilePicture] = useState<any>(undefined);
+
     const URLUpload = window.location.href.includes('http://localhost:3000/')
         ? 'http://localhost:5000/users/upload'
         : 'https://www.pilexlaflex.com:5002/users/upload';
 
+    const URLProfilePicture = window.location.href.includes('http://localhost:3000/')
+        ? 'http://localhost:5000/users/profilePicture'
+        : 'https://www.pilexlaflex.com:5002/users/profilePicture';
+
+    const getProfilePicture = async () => {
+        try {
+            const response = await doRequest('get', URLProfilePicture);
+            setProfilePicture(response.data.profilepicture);
+            console.log('profilePicture :>> ', profilePicture);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const getURL = (url: string | File) => {
+        if (typeof url === 'string') {
+            return url;
+        } else {
+            return URL.createObjectURL(url);
+        }
+    };
+
     const upload = async (file: File) => {
+        console.log('file :>> ', file);
+        setProfilePicture(file);
         const formData = new FormData();
         formData.append('profilePicture', file);
 
@@ -23,6 +49,10 @@ export default function DashboardHeaderInfo(userData: EmployeeProfileType) {
             console.log(e);
         }
     };
+
+    useEffect(() => {
+        getProfilePicture();
+    }, []);
 
     return (
         <div className="d-flex flex-row">
@@ -53,7 +83,25 @@ export default function DashboardHeaderInfo(userData: EmployeeProfileType) {
                             justifyContent: 'center',
                         }}
                     >
-                        <FontAwesomeIcon icon={faUpload} />
+                        {profilePicture ? (
+                            <img
+                                src={getURL(profilePicture)}
+                                alt="profile"
+                                style={{
+                                    background: 'rgba(0,0,0,.122)',
+                                    borderRadius: '50%',
+                                    height: '60px',
+                                    width: '60px',
+                                    margin: '0 10px',
+                                    cursor: 'pointer',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            />
+                        ) : (
+                            <FontAwesomeIcon icon={faUpload} />
+                        )}
                     </label>
                 </div>
             </div>
